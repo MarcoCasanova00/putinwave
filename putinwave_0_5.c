7aefffec783d0a0f277909db939004d2747762bc
MarcoCasanova00 <94206717+MarcoCasanova00@users.noreply.github.com>
2021-11-14 11:47:32 -0500

    Add files via upload

---
+++ putinwave_0_5.c
@@ -0,0 +1,58 @@
+/******************************************************************************
+
+                            KGB STENOGRAPHY
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+FILE *wav, *tohide, *final;
+char bit [1], bit2[1], byte[1], other[100];
+
+int main (void) {
+int hide () {
+    int a=0;
+    long count, c=0;
+    scanf("%d", &count);
+    printf("Reading file, please wait...\n");
+    
+    for (int b=0; b<44;b++) {
+            fread(byte, 1, 1, final);
+    }
+    while (c<count) {
+        if (a!=100000) {
+            fread(bit, 1, 1, wav);
+            fwrite(bit, 1, 1, final);
+            a++;
+        } else {
+            if (count-c>100) {
+                fread(other, 100, 1, wav);
+                fread(other, 100, 1, tohide);
+                fwrite(other, 100, 1, final);
+                a=0;
+                c=c+100;
+            }
+            else {
+                fread(bit,1, 1, wav);
+                fread(bit2,1, 1, tohide);
+                fwrite(bit2, 1, 1, final);
+                a=0;
+                c++;
+            }
+        }
+    }
+    while (!feof(wav)) {
+        fread(bit, 1, 1, wav);
+        fwrite(bit, 1 , 1, final);
+    }
+;
+
+int extract() {
+    int a=0;
+    long count, c=0;
+}
+}
+
+return 0;
+}
